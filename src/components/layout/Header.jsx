316663e6363a465fc0e333426b56ecc5936c28bb
soydouglas <soydouglass@gmail.com>
2025-07-15 23:32:20 -0500

    Header for desktop: done

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,22 +1,26 @@
 import LogoFission from '../../assets/LogoFission.svg'
 import '../css/Header.css'
+import '../../../src/index.css'
 
 function Header() {
     return (
         <>
             <header className='Header'>
-                <nav>
-                    <a href="" className='HeaderImg'>
-                        <img src={LogoFission} alt="Fission logo" />
-                    </a>
-                    <a href="">FISSION</a>
-                    <a href="">Home</a>
-                    <a href="">Servicios</a>
-                    <a href="">Sobre nosotros</a>
-                    <a href="">Contacto</a>
-                    <a href="">
-                        <button>Agenda una llamada</button>
-                    </a>
+                <nav className='HeaderNav'>
+                    <div className='navLeft'>
+                        <a href="" className='HeaderImg'>
+                            <img src={LogoFission} alt="Fission logo" />
+                        </a>
+                        <a href="" className='logoText'>FISSION</a>
+                    </div>
+                    <div className='navRight'>
+                        <a href="">Nuestros Servicios</a>
+                        <a href="">Sobre nosotros</a>
+                        <a href="">Contacto</a>
+                        <a href="">
+                            <button>CONTÁCTANOS</button>
+                        </a>
+                    </div>
                 </nav>
             </header>
         </>

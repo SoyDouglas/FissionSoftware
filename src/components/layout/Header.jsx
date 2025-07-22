64c44ea5805690dc6ce1e937da87fd251caccd32
soydouglas <soydouglass@gmail.com>
2025-07-22 18:26:43 -0500

    New font family

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -43,7 +43,7 @@ function Header() {
                             < ArrowDown className="arrow"/>
                         </a>
                         <a href="">
-                            <button>Contáctanos</button>
+                            <button>Agenda una llamada</button>
                         </a>
                     </div>
                 </nav>
--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -3,7 +3,9 @@ import '../css/Hero.css';
 function Hero() {
     return (
         <section className='Hero'>
-            <h1>Hello Fission!</h1>
+            <h2>Escala tu negocio con soluciones</h2>
+            <h1>IA Generativa</h1>
+            <h3>De la idea al MVP en 30 días. Primer sprint o gratis</h3>
         </section>
     );
 }

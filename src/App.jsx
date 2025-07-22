7c788293ddb823da93b09fceb0513c5f1cdda31b
soydouglas <soydouglass@gmail.com>
2025-07-22 18:19:31 -0500

    Adding Hero.jsx and change in font weight for nav elements of header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/layout/Header"
+import Hero from "./components/layout/Hero"
 
 function App() {
 
   return (
     <>
       <Header />
+      <Hero />
     </>
   )
 }
---
+++ src/components/layout/Hero.jsx
@@ -0,0 +1,11 @@
+import '../css/Hero.css'; 
+
+function Hero() {
+    return (
+        <section className='Hero'>
+            <h1>Hello Fission!</h1>
+        </section>
+    );
+}
+
+export default Hero

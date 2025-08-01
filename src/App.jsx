0a9f8b06ae401f1d9de8189849785b693092ed44
soydouglas <soydouglass@gmail.com>
2025-07-31 21:25:08 -0500

    Creating new section HowWeDo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/layout/Header"
 import Hero from "./components/layout/Hero"
 import WhatWeDo from './components/layout/WhatWeDo'
+import HowWeDo from "./components/layout/HowWeDo"
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Hero />
       <WhatWeDo />
+      <HowWeDo />
     </>
   )
 }
---
+++ src/components/layout/HowWeDo.jsx
@@ -0,0 +1,11 @@
+import '../css/HowWeDo.css'
+
+function HowWeDo(){
+    return (
+        <section className='HowWeDo'>
+            <h1>Nuestro proceso</h1>
+        </section>
+    )
+}
+
+export default HowWeDo;
